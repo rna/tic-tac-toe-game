80448fc83e85f308116d2557d113684a8cac2a9a
Sharmarke <somaliwarsan@gmail.com>
2019-11-13 01:24:16 +0300

    added UI for user input and representation of the game board

--- bin/main.rb
+++ bin/main.rb
@@ -11,19 +11,62 @@ while counter
   if active_player.odd?
     puts "Player-#{active_player}: Please enter box number:-"
     box = gets.chomp.to_i
-    arr = box > 6 ? 2 : (box > 3 ? 1 : 0)
-    board[arr][box] = 'X'
+    arr = if box > 6
+            2
+          elsif box > 3
+            1
+          else
+            0
+          end
+
+    if (box == 1) || (box == 4) || (box == 7)
+      cell = 0
+    elsif (box == 2) || (box == 5) || (box == 8)
+      cell = 1
+    elsif (box == 3) || (box == 6) || (box == 9)
+      cell = 2
+    end
+    board[arr][cell] = 'X'
     active_player += 1
     counter += 1
-    print board
+    puts '----------------'
+    board.each do |b|
+      b.each do |c|
+        print "| #{c} |"
+      end
+      puts ' '
+    end
+    puts '----------------'
   end
 
   next unless active_player.even?
-    puts "Player-#{active_player}: Please enter box number:-"
-    box = gets.chomp.to_i
-    arr = box > 6 ? 2 : (box > 3 ? 1 : 0)
-    board[arr][box] = 'O'
-    active_player -= 1
-    counter += 1
-    print board
+
+  puts "Player-#{active_player}: Please enter box number:-"
+  box = gets.chomp.to_i
+  arr = if box > 6
+          2
+        elsif box > 3
+          1
+        else
+          0
+        end
+
+  if (box == 1) || (box == 4) || (box == 7)
+    cell = 0
+  elsif (box == 2) || (box == 5) || (box == 8)
+    cell = 1
+  elsif (box == 3) || (box == 6) || (box == 9)
+    cell = 2
+  end
+  board[arr][cell] = 'O'
+  active_player -= 1
+  counter += 1
+  puts '----------------'
+  board.each do |b|
+    b.each do |c|
+      print "| #{c} |"
+    end
+    puts ' '
+  end
+  puts '----------------'
 end
