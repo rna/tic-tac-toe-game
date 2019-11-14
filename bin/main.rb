143d1c0bbcc4f74d9ac1d7c8a2c99f7167022080
Sharmarke <somaliwarsan@gmail.com>
2019-11-14 15:50:14 +0300

    added play again method

--- bin/main.rb
+++ bin/main.rb
@@ -1,11 +1,9 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-$board
-
-def display_board
+def display_board(board)
   puts '----------------'
-  $board.each do |sub|
+  board.each do |sub|
     sub.each do |c|
       print "| #{c} |"
     end
@@ -14,16 +12,33 @@ def display_board
   puts '----------------'
 end
 
+def play_again
+  puts "Would you like to start again?"
+  puts "1. Yes"
+  puts "2. No"
+  choice = gets.chomp.to_i
+
+  case choice
+    when 1
+      game
+    when 2
+      exit
+    else
+      puts "Please enter a valid choice"
+  end
+end
+
 def game
-  $board = [%w[1 2 3], %w[4 5 6], %w[7 8 9]]
+  board = [%w[1 2 3], %w[4 5 6], %w[7 8 9]]
   active_player = 1
   # result = false
 
   counter = 0
-  endgame = false
+  game_on = true
 
-  while counter < 9
-
+  while game_on
+    
+    display_board(board)
     puts "Player-#{active_player}: Please enter box number:-"
     box = gets.chomp.to_i
     arr = if box > 6
@@ -41,29 +56,30 @@ def game
     elsif (box == 3) || (box == 6) || (box == 9)
       cell = 2
     end
-    $board[arr][cell] = active_player.even? ? 'O' : 'X'
+    board[arr][cell] = active_player.even? ? 'O' : 'X'
     counter += 1  
     
     # Checking the game winning status
     # Horizontal Win check
 
-    $board.each do |sub|
+    board.each_with_index do |sub, i|
       if sub.all? { |e| e == 'O' }
-        display_board
-        endgame = true
+        display_board(board)
+        game_on = false
         puts 'Player 2 wins'
+        play_again
         break
       elsif sub.all? { |e| e == 'X' }
-        display_board
-        endgame = true
+        display_board(board)
+        game_on = false
         puts 'Player 1 wins'
+        play_again
         break
       elsif counter >= 9
-        display_board
+        display_board(board)
+        game_on = false
         puts 'Game is drawn'
-        break
-      else
-        display_board
+        play_again
         break
       end
     end
@@ -74,26 +90,7 @@ def game
     else
       active_player += 1
     end
-
-    if endgame
-      break
-    end
   end
 end
 
-
 game
-
-puts "Would you like to start again?"
-puts "1. Yes"
-puts "2. No"
-choice = gets.chomp.to_i
-
-case choice
-  when 1
-    game
-  when 2
-    exit
-  else
-    puts "Please enter a valid choice"
-end
