14b39b94b9f2d05396b54c756f5ec4904f96e25a
Ramesh <rameshnaidu.allu@gmail.com>
2019-11-16 02:02:32 +0530

    fix linter issues

--- lib/logic.rb
+++ lib/logic.rb
@@ -51,14 +51,14 @@ class Game
   end
 
   def winning_board
-    winning_board = [[@board[0][0], @board[0][1], @board[0][2]],
-                     [@board[1][0], @board[1][1], @board[1][2]],
-                     [@board[2][0], @board[2][1], @board[2][2]],
-                     [@board[0][0], @board[1][0], @board[2][0]],
-                     [@board[0][1], @board[1][1], @board[2][1]],
-                     [@board[0][2], @board[1][2], @board[2][2]],
-                     [@board[0][0], @board[1][1], @board[2][2]],
-                     [@board[0][2], @board[1][1], @board[2][0]]]
+    [[@board[0][0], @board[0][1], @board[0][2]],
+     [@board[1][0], @board[1][1], @board[1][2]],
+     [@board[2][0], @board[2][1], @board[2][2]],
+     [@board[0][0], @board[1][0], @board[2][0]],
+     [@board[0][1], @board[1][1], @board[2][1]],
+     [@board[0][2], @board[1][2], @board[2][2]],
+     [@board[0][0], @board[1][1], @board[2][2]],
+     [@board[0][2], @board[1][1], @board[2][0]]]
   end
 
   def check_status
